980515d747c0956c6cf4487693cb69206c5c01db
hello-express <liuhaobin486@gmail.com>
2022-09-23 23:16:09 +0800

    finish params

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Users from '../components/user/Users'
 import Rights from '../components/power/Rights'
 import Roles from '../components/power/Roles'
 import Cate from '../components/goods/Cate'
+import Params from '../components/goods/params'
 Vue.use(Router)
 
 const router = new Router({
@@ -30,6 +31,10 @@ const router = new Router({
         {
           path: '/categories',
           component: Cate
+        },
+        {
+          path: '/params',
+          component: Params
         },
         {
           path: '/rights',
